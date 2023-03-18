09ebf2a7a65fda7dae7cd53baa7995c66491ace0
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-03-18 16:50:51 +1100

    Complete game functionality including input filter

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 function computerChoice() {
   let x = Math.floor(Math.random() * 3);
   if (x === 0) {
-    y = "Scissors";
+    y = "SCISSORS";
   } else if (x === 1) {
-    y = "Paper";
+    y = "PAPER";
   } else {
-    y = "Rock";
+    y = "ROCK";
   }
   return y;
 }
-console.log(computerChoice());
+
+function playerChoice() {
+  let y = prompt("Your selection: ");
+  let x = y.toUpperCase();
+  return x;
+}
+
+function winner() {
+  if (c === p) {
+    console.log("TIE!");
+  } else if (c === "SCISSORS" && p === "ROCK") {
+    console.log("You Win! Rock beats Scissors!");
+  } else if (c === "PAPER" && p === "SCISSORS") {
+    console.log("You Win! Scissors beats Paper!");
+  } else if (c === "ROCK" && p === "PAPER") {
+    console.log("You Win! Paper beats Rock!");
+  } else {
+    console.log(`You Lose! ${c} beats ${p}!`);
+  }
+}
+let p = playerChoice();
+while (!(p == "ROCK" || p == "SCISSORS" || p == "PAPER")) {
+  p = playerChoice();
+}
+
+let c = computerChoice();
+
+console.log(p);
+console.log(c);
+winner();
