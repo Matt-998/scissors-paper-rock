c39725bf65dc4630d998dc27b229a72d9d29a38d
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-03-19 14:53:24 +1100

    Complete playRound and game functions, now fully operational

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+game();
+
 function computerChoice() {
   let x = Math.floor(Math.random() * 3);
   if (x === 0) {
@@ -16,26 +18,54 @@ function playerChoice() {
   return x;
 }
 
-function winner() {
+function winner(c, p) {
   if (c === p) {
     console.log("TIE!");
   } else if (c === "SCISSORS" && p === "ROCK") {
     console.log("You Win! Rock beats Scissors!");
+    return 1;
   } else if (c === "PAPER" && p === "SCISSORS") {
     console.log("You Win! Scissors beats Paper!");
+    return 1;
   } else if (c === "ROCK" && p === "PAPER") {
     console.log("You Win! Paper beats Rock!");
+    return 1;
   } else {
     console.log(`You Lose! ${c} beats ${p}!`);
+    return 2;
   }
 }
-let p = playerChoice();
-while (!(p == "ROCK" || p == "SCISSORS" || p == "PAPER")) {
-  p = playerChoice();
+
+function playRound() {
+  let c = computerChoice();
+  let p = playerChoice();
+  while (!(p == "ROCK" || p == "SCISSORS" || p == "PAPER")) {
+    p = playerChoice();
+  }
+  console.log(`Player - ${p}`);
+  console.log(`Computer - ${c}`);
+  let score = winner(c, p);
+  return score;
 }
 
-let c = computerChoice();
-
-console.log(p);
-console.log(c);
-winner();
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  console.log("Scissors Paper Rock - Best of 5!");
+  for (let count = 0; count < 5; count++) {
+    let score = playRound();
+    if (score === 1) {
+      playerScore++;
+    } else if (score === 2) {
+      computerScore++;
+    }
+    console.log(`Player - ${playerScore}\nComputer - ${computerScore}`);
+  }
+  if (playerScore > computerScore) {
+    console.log("Congratulations! You WIN!");
+  } else if (playerScore < computerScore) {
+    console.log("Bad luck! You LOSE!");
+  } else {
+    console.log("It's a TIE! Who made this???");
+  }
+}
