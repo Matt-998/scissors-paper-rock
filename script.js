cfd4d8431204a0454cde839d782db6eaffa69f5a
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-04-01 12:44:01 +1100

    Make game end after best of 5 by disabling buttons
    Need to add a reset button now, figure thats a nicer touch than just
    refreshing the page for a new game.

--- script.js
+++ script.js
@@ -1,46 +1,41 @@
-const scissors = document.querySelector("#scissors");
+const choiceList = document.querySelectorAll("button.choices");
 const paper = document.querySelector("#paper");
 const rock = document.querySelector("#rock");
 const displayWinner = document.querySelector("#winner");
 const playerScoreDisplay = document.querySelector("#player");
 const computerScoreDisplay = document.querySelector("#computer");
+const playerChoiceDisplay = document.querySelector("#playerChoiceDisplay");
+const computerChoiceDisplay = document.querySelector("#computerChoiceDisplay");
 
-scissors.onclick = playRound;
-paper.onclick = playRound;
-rock.onclick = playRound;
+// To do - make game end
+for (const choice of choiceList) {
+  choice.addEventListener("click", playRound);
+}
+
+let roundCount = 0;
 let playerScore = 0;
 let computerScore = 0;
 playerScoreDisplay.textContent = `Player: ${playerScore}`;
 computerScoreDisplay.textContent = `Computer: ${computerScore}`;
 
-console.log("Scissors Paper Rock - Best of 5!");
 displayWinner.textContent = "Scissors Paper Rock - Best of 5!";
 
 // Check for winner with computer and player inputs
 function decideWinner(computerChoice, playerChoice) {
   if (computerChoice === playerChoice) {
-    console.log("TIE!");
     displayWinner.textContent = "TIE!";
   } else if (computerChoice === "SCISSORS" && playerChoice === "ROCK") {
-    console.log("You Win! Rock beats Scissors!");
     displayWinner.textContent = "You Win! Rock beats Scissors!";
     playerScoreDisplay.textContent = `Player: ${playerScore++}`;
-    return 1;
   } else if (computerChoice === "PAPER" && playerChoice === "SCISSORS") {
-    console.log("You Win! Scissors beats Paper!");
     displayWinner.textContent = "You Win! Scissors beats Paper!";
     playerScoreDisplay.textContent = `Player: ${playerScore++}`;
-    return 1;
   } else if (computerChoice === "ROCK" && playerChoice === "PAPER") {
-    console.log("You Win! Paper beats Rock!");
     displayWinner.textContent = "You Win! Paper beats Rock!";
     playerScoreDisplay.textContent = `Player: ${playerScore++}`;
-    return 1;
   } else {
-    console.log(`You Lose! ${computerChoice} beats ${playerChoice}!`);
     displayWinner.textContent = `You Lose! ${computerChoice} beats ${playerChoice}!`;
     computerScoreDisplay.textContent = `Computer: ${computerScore++}`;
-    return 2;
   }
 }
 
@@ -48,25 +43,25 @@ function decideWinner(computerChoice, playerChoice) {
 function playRound() {
   let computerChoice = getComputerChoice();
   let playerChoice = this.value;
-  console.log(`Player - ${playerChoice}`);
-  console.log(`Computer - ${computerChoice}`);
-  let winner = decideWinner(computerChoice, playerChoice);
+  playerChoiceDisplay.textContent = `You chose ${playerChoice}`;
+  computerChoiceDisplay.textContent = `Computer chose ${computerChoice}`;
+  decideWinner(computerChoice, playerChoice);
   playerScoreDisplay.textContent = `Player: ${playerScore}`;
   computerScoreDisplay.textContent = `Computer: ${computerScore}`;
-  return winner;
+  roundCount++;
+  if (roundCount === 5) {
+    for (choice of choiceList) {
+      choice.disabled = true;
+      if (playerScore > computerScore) {
+        displayWinner.textContent = "Congratulations! You WIN!";
+      } else if (playerScore < computerScore) {
+        displayWinner.textContent = "Bad luck! You LOSE!";
+      } else if (playerScore === computerScore) {
+        displayWinner.textContent = "It's a TIE! Who made this???";
+      }
+    }
+  }
 }
-
-// Plays 5 rounds and declares a winner
-
-// console.log(`Player - ${playerScore}\nComputer - ${computerScore}`);
-
-// if (playerScore > computerScore) {
-//   console.log("Congratulations! You WIN!");
-// } else if (playerScore < computerScore) {
-//   console.log("Bad luck! You LOSE!");
-// } else {
-//   console.log("It's a TIE! Who made this???");
-// }
 
 // Get computer choice with RNG
 function getComputerChoice() {
